03383cc7d1067895035fcb6a467f8a960ed2c0b2
atharvabehar <69201688+atharvabehar@users.noreply.github.com>
2021-08-30 13:53:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCk7ig62XY_zK-2-_UOM6tcLCIiiSwaZdQ",
+  authDomain: "story-e4780.firebaseapp.com",
+  projectId: "story-e4780",
+  storageBucket: "story-e4780.appspot.com",
+  messagingSenderId: "763568318490",
+  appId: "1:763568318490:web:cf502df11eb766c649c9a3"
+}; 
