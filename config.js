7fe7d41ed3bdb2dc0560a8172b4d005dfe789d47
atharvabehar <69201688+atharvabehar@users.noreply.github.com>
2021-09-04 15:01:51 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCk7ig62XY_zK-2-_UOM6tcLCIiiSwaZdQ",
-  authDomain: "story-e4780.firebaseapp.com",
-  projectId: "story-e4780",
-  storageBucket: "story-e4780.appspot.com",
-  messagingSenderId: "763568318490",
-  appId: "1:763568318490:web:cf502df11eb766c649c9a3"
+  apiKey: "AIzaSyCevJTVSukiJ3VwYXKKnOjMlVDh2_fyB9Q",
+  authDomain: "spectagram-733b7.firebaseapp.com",
+  projectId: "spectagram-733b7",
+  storageBucket: "spectagram-733b7.appspot.com",
+  messagingSenderId: "698091498981",
+  appId: "1:698091498981:web:e62db93b92a9fdbb82d30c"
 }; 
